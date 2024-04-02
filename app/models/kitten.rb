cce1374bbca06a3b9d503d5e0aa1807c0cb86bbb
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-04-02 15:34:22 -0700

    Create all prelim methods for kitten controller

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,43 @@
 class Kitten < ApplicationRecord
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to kitten_path(@kitten.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
