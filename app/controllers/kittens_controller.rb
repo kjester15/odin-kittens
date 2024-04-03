9ff471ec4dd699e3496ccaecfce60a7c6dbaaddb
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-04-02 17:07:46 -0700

    Add and slightly format flash messages for creating, editing, and deleting

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -7,8 +7,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
-      redirect_to kitten_path(@kitten.id)
+      redirect_to kitten_path(@kitten.id), notice: "You created a kitten!"
     else
+      flash.now[:error] = "You suck at creating kittens :("
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +30,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
-      redirect_to @kitten
+      redirect_to @kitten, notice: "You updated a kitten!"
     else
+      flash.now[:error] = "You suck at creating kittens :("
       render :edit, status: :unprocessable_entity
     end
   end
@@ -38,7 +40,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
-    redirect_to kittens_path
+    redirect_to kittens_path, notice: "You deleted a kitten. You sick bastard..."
   end
 
   private
